b9b079753a1a6db0969563e53bf85439c7f6bbb7
Lucas Trigo <lucasetrigo@gmail.com>
2022-09-17 18:35:49 -0300

    First Commit

---
+++ src/components/WorkData.js
@@ -0,0 +1,40 @@
+export const Work = [
+    {
+        id: 1,
+        name: "Cambursano Minting DApp",
+        description:
+            "NFT Minting Website, where users can mint their own NFTs from the Cambursano Collection (which was created by me). Users will be prompted to connect their wallet to the website, and then they can mint their NFTs.",
+        tags: ["React Js", "Solidity", "Web3", "IPFS", "Ethereum", "WebPack"],
+
+        demo: "https://cheerful-eclair-14d93f.netlify.app/",
+        github: "https://github.com/LucasETrigo/Cambursano-NFT-Project",
+    },
+    {
+        id: 2,
+        name: "NFTxHype",
+        description:
+            "NFT E-Commerce App made with react js.",
+        tags: ["React Js", "Javascript", "Html", "CSS"],
+
+        demo: "https://github.com/LucasETrigo/ecom-react-app",
+        github: "https://github.com/LucasETrigo/ecom-react-app",
+    },
+    {
+        id: 3,
+        name: "Working On React Ecommerce WebSite",
+        description: "An NFT Marketplace made with React.js, upcoming...",
+        tags: ["react", "api", "sass"],
+
+        demo: "https://github.com/LucasETrigo/ecom-react-app",
+        github: "https://github.com/LucasETrigo/ecom-react-app",
+    },
+    {
+        id: 3,
+        name: "My Resume",
+        description: "Here you can take a look at my full resume.",
+        tags: ["Resume"],
+
+        demo: "https://drive.google.com/file/d/1eIg8t4lNECre5DIvix1CS6Rj6mRDoDXJ/view?usp=sharing",
+        github: "https://drive.google.com/file/d/1eIg8t4lNECre5DIvix1CS6Rj6mRDoDXJ/view?usp=sharing",
+    },
+];
